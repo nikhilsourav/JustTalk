d8fd9508a37c0f8bb6a28ab15f3cb8677eb6b908
Nikhil Sourav <nikhil.rj900@gmail.com>
2020-08-08 16:36:56 +0530

    JustTalk v.1.0.0

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,7 @@
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector('.chat-messages');
+const roomName = document.getElementById('room-name')
+const userList = document.getElementById('users')
 
 // Get username and room from url
 const {username, room } = Qs.parse(location.search, {
@@ -10,6 +12,12 @@ const socket = io();
 
 // Join chatroom
 socket.emit('Join Room', {username, room});
+
+// Get room and users
+socket.on('roomUsers', ({room, users}) => {
+  outputRoomName(room);
+  outputUsers(users);
+})
 
 // Message from server
 socket.on("message", (message) => {
@@ -45,4 +53,16 @@ function outputMessage(message) {
       ${message.text}
     </p>`;
   document.querySelector(".chat-messages").appendChild(div);
+}
+
+// Add roomname to DOM
+function outputRoomName(room){
+  roomName.innerText = room;
+}
+
+// Add users to DOM
+function outputUsers(users){
+  userList.innerHTML = `
+    ${users.map(user => `<li>${user.username}</li>`).join('')}
+  `;
 }
--- server.js
+++ server.js
@@ -43,6 +43,12 @@ io.on("connection", (socket) => {
         formatMessage(botName, `${user.username} has joined the chat`)
       );
 
+    // Send users and room info
+    io.to(user.room).emit("roomUsers", {
+      room: user.room,
+      users: getRoomUsers(user.room),
+    });
+
     // Listen for chat message
     socket.on("chatMessage", (msg) => {
       const user = getCurrentUser(socket.id);
@@ -58,6 +64,12 @@ io.on("connection", (socket) => {
           "message",
           formatMessage(botName, `${user.username} has left the chat`)
         );
+
+        // Send users and room info
+        io.to(user.room).emit("roomUsers", {
+          room: user.room,
+          users: getRoomUsers(user.room),
+        });
       }
     });
   });
