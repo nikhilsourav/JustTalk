42ad15d2785c90a9fb32a2ccc5e46d4b6dcfb6e8
Nikhil Sourav <nikhil.rj900@gmail.com>
2020-08-08 15:26:53 +0530

    setting up basic Join and Leave

--- public/js/main.js
+++ public/js/main.js
@@ -1,7 +1,15 @@
 const chatForm = document.getElementById("chat-form");
 const chatMessages = document.querySelector('.chat-messages');
 
+// Get username and room from url
+const {username, room } = Qs.parse(location.search, {
+    ignoreQueryPrefix: true
+})
+
 const socket = io();
+
+// Join chatroom
+socket.emit('Join Room', {username, room});
 
 // Message from server
 socket.on("message", (message) => {
--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const http = require("http");
 const express = require("express");
 const socketio = require("socket.io");
 const formatMessage = require("./utils/messages");
+const { userJoin, getCurrentUser } = require("./utils/users");
+const { count } = require("console");
 const app = express();
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
@@ -15,31 +17,39 @@ app.use(express.static(path.join(__dirname, "public")));
 const botName = "JustTalk bot";
 // run when client connects
 io.on("connection", (socket) => {
-  console.log("New ws connection...");
+  socket.on("Join Room", ({ username, room }) => {
+    const user = userJoin(socket.id, username, room);
 
-  // Welcome current user
-  socket.emit(
-    "message",
-    formatMessage(
-      botName,
-      "Welcome to JustTalks a website designed and developed by Nikhil"
-    )
-  );
+    socket.join(user.room);
 
-  // Notify when a user connects
-  socket.broadcast.emit(
-    "message",
-    formatMessage(botName, "A user has joined the chat")
-  );
+    // Welcome current user
+    socket.emit(
+      "message",
+      formatMessage(
+        botName,
+        "Welcome to JustTalks a website designed and developed by Nikhil Sourav"
+      )
+    );
+    // Notify when a user connects
+    socket.broadcast
+      .to(user.room)
+      .emit(
+        "message",
+        formatMessage(botName, `${user.username} has joined the chat`)
+      );
 
-  // Notify when a user disconnects
-  socket.on("disconnect", () => {
-    io.emit("message", formatMessage(botName, "A user has left the chat"));
-  });
+    // Listen for chat message
+    socket.on("chatMessage", (msg) => {
+      io.emit("message", formatMessage("user", msg));
+    });
 
-  // Listen for chat message
-  socket.on("chatMessage", (msg) => {
-    io.emit("message", formatMessage('user', msg));
+    // Notify when a user disconnects
+    socket.on("disconnect", () => {
+      io.emit(
+        "message",
+        formatMessage(botName, `${user.username} has left the chat`)
+      );
+    });
   });
 });
 
