637be2707419cf33c88fa5c18a4d5021bbaba92a
Nikhil Sourav <nikhil.rj900@gmail.com>
2020-08-08 13:24:24 +0530

    Basic messaging functionality

---
+++ public/js/main.js
@@ -0,0 +1,16 @@
+const chatForm = document.getElementById('chat-form')
+
+const socket = io();
+
+socket.on('message', message=> console.log(message))
+
+// Message submit
+chatForm.addEventListener('submit', e => {
+    e.preventDefault();
+
+    // Get message text
+    const msg = e.target.elements.msg.value;
+
+    // Emit message to server
+    socket.emit('chatMessage', msg);
+})
--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 // setup
 const path = require("path");
-const http = require("http")
+const http = require("http");
 const express = require("express");
-const socketio = require("socket.io")
+const socketio = require("socket.io");
 const app = express();
-const io = socketio(server)
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
+const io = socketio(server);
 
 // serve static folder
 app.use(express.static(path.join(__dirname, "public")));
 
 // run when client connects
-io.on('connection', socket => {
-    console.log("New ws connection")
-})
+io.on("connection", (socket) => {
+  console.log("New ws connection...");
+
+  // Welcome current user
+  socket.emit("message", "welcome to JustTalk");
+
+  // Notify when a user connects
+  socket.broadcast.emit("message", "A user has joined the chat");
+  
+  // Notify when a user disconnects
+  socket.on('disconnect', () => {
+    io.emit('message', 'A user has left the chat');
+  })
+
+  // Listen for chat message
+  socket.on('chatMessage', msg => {
+    io.emit('message', msg)
+  })
+});
 
 // Listen on server
 server.listen(port, () =>
